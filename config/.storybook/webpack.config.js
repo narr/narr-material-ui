959966e874cee990281688c9d46a2a941756ef9a
narr <nardgu@gmail.com>
2018-07-07 02:03:28 +0800

    refactor configs for Jest and Storybook

--- config/.storybook/webpack.config.js
+++ config/.storybook/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const commonConfig = require('../../packages/narr-material-ui-common');
 
-const includePath = path.resolve(__dirname, '../../packages');
-const tsconfigPath = path.resolve(__dirname, '../../tsconfig.json');
+const includePath = path.join(__dirname, '../../packages');
+const tsconfigPath = path.join(__dirname, '../../tsconfig.json');
 
 module.exports = commonConfig.getStorybookWebpackConfig({
   includePath,
--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -1,6 +1,7 @@
 const commonConfig = require('../../packages/narr-material-ui-common');
 
 module.exports = commonConfig.getJestConfig({
-  testRootDir: 'packages',
-  collectCoverageFromRootDir: 'packages/**/src',
+  testRootDir: './packages',
+  tsConfigFile: './tsconfig.json',
+  collectCoverageFromRootDir: './packages/**/src',
 });
