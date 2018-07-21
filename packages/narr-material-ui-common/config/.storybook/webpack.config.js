8855214cb05306ef33053d5b14add749cf2dc1df
narr <nardgu@gmail.com>
2018-07-21 12:48:21 +0800

    fix(pkg-common): make a compiled js from Typescript available in Storybook
    #41

--- packages/narr-material-ui-common/config/.storybook/webpack.config.js
+++ packages/narr-material-ui-common/config/.storybook/webpack.config.js
@@ -1,25 +1,48 @@
 const path = require('path');
+const _ = require('lodash');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const defaultTsconfigPath = path.join(__dirname, '../tsconfig.json');
 
+function removeStorybookBabelLoader(rules) {
+  const babelLoaderTestRegexString = /\.jsx?$/.toString();
+  return _.filter(rules, rule => {
+    return rule.test.toString() !== babelLoaderTestRegexString;
+  });
+}
+
 module.exports = ({ includePath, tsconfigPath = defaultTsconfigPath } = {}) => {
   return (baseConfig, env, defaultConfig) => {
-    // Extend defaultConfig as you need.
-
-    // For example, add typescript loader:
-    defaultConfig.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      include: includePath,
-      use: [
-        {
-          loader: 'ts-loader',
-          options: {
-            configFile: tsconfigPath,
-            transpileOnly: true,
+    defaultConfig.module.rules = removeStorybookBabelLoader(
+      defaultConfig.module.rules
+    ).concat([
+      {
+        test: /\.(ts|tsx)$/,
+        include: includePath,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: tsconfigPath,
+              transpileOnly: true,
+            },
           },
-        },
-      ],
-    });
+        ],
+      },
+      {
+        test: /\.js$/,
+        include: includePath,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['react-app'],
+            },
+          },
+        ],
+      },
+    ]);
+
     defaultConfig.resolve.extensions.push('.ts', '.tsx');
     defaultConfig.plugins.push(
       new ForkTsCheckerWebpackPlugin({
